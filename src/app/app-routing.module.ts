23b5a7c054a2b425eff0a2b53e18936a4f649f6a
jamesXFY <45582531+jamesXFY@users.noreply.github.com>
2020-02-02 09:26:54 +1000

    add home content

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,11 +2,14 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { UsersComponent } from './components/users/users.component';
 import { CardsComponent } from './components/cards/cards.component';
+import { HomeComponent } from './components/home/home.component';
 
 
 const routes: Routes = [
+  { path: 'home', component: HomeComponent },
   { path: 'usersTable', component: UsersComponent },
-  { path: 'usersCard', component: CardsComponent }
+  { path: 'usersCard', component: CardsComponent },
+  { path: '', redirectTo: '/home', pathMatch: 'full'}
 ];
 
 @NgModule({
--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,33 +1,38 @@
-import { Component, OnInit} from '@angular/core';
-import {trigger, state, style, animate, transition} from '@angular/animations';
-
-declare let componentHandler: any;
+import { Component, OnInit } from "@angular/core";
+import {
+  trigger,
+  state,
+  style,
+  animate,
+  transition
+} from "@angular/animations";
 
 @Component({
-    templateUrl: 'home.component.html',
-    animations: [
-        trigger('flyInOut', [
-            state('in', style({ opacity: 1, transform: 'translateX(0)' })),
-            transition('void => *', [
-                style({
-                    opacity: 0,
-                    transform: 'translateX(-100%)'
-                }),
-                animate('0.6s ease-in')
-            ]),
-            transition('* => void', [
-                animate('0.2s 10 ease-out', style({
-                    opacity: 0,
-                    transform: 'translateX(100%)'
-                }))
-            ])
-        ])
-    ]
+  selector: "app-home-component",
+  templateUrl: "./home.component.html",
+  styleUrls: ["./home.component.css"],
+  animations: [
+    trigger("flyInOut", [
+      state("in", style({ opacity: 1, transform: "translateX(0)" })),
+      transition("void => *", [
+        style({
+          opacity: 0,
+          transform: "translateX(-100%)"
+        }),
+        animate("0.6s ease-in")
+      ]),
+      transition("* => void", [
+        animate(
+          "0.2s ease-out",
+          style({
+            opacity: 0,
+            transform: "translateX(100%)"
+          })
+        )
+      ])
+    ])
+  ]
 })
 export class HomeComponent {
-
-    constructor() {
-
-    }
-
+  constructor() {}
 }
