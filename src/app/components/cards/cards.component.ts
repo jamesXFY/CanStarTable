6973db49138ae929994032ff5aad605d062dfd38
jamesXFY <45582531+jamesXFY@users.noreply.github.com>
2020-02-02 10:03:06 +1000

    add animate

--- src/app/components/cards/cards.component.ts
+++ src/app/components/cards/cards.component.ts
@@ -2,11 +2,33 @@ import { Component, OnInit } from '@angular/core';
 import { Observable } from 'rxjs';
 import { IUser } from 'src/app/shared/interfaces';
 import { FakeBackendService } from 'src/app/shared/services/fakebackend.service';
+import { trigger, state, style, transition, animate } from '@angular/animations';
 
 @Component({
   selector: 'app-cards',
   templateUrl: './cards.component.html',
-  styleUrls: ['./cards.component.css']
+  styleUrls: ['./cards.component.css'],
+  animations: [
+    trigger("flyInOut", [
+      state("in", style({ opacity: 1, height: '0' })),
+      transition("void => *", [
+        style({
+          opacity: 0,
+          height: '100%'
+        }),
+        animate("0.6s ease-in")
+      ]),
+      transition("* => void", [
+        animate(
+          "0.2s ease-out",
+          style({
+            opacity: 0,
+            height: '100%'
+          })
+        )
+      ])
+    ])
+  ]
 })
 export class CardsComponent implements OnInit {
 
--- src/app/components/users/users.component.ts
+++ src/app/components/users/users.component.ts
@@ -5,12 +5,34 @@ import { NgbModal, NgbModalRef } from '@ng-bootstrap/ng-bootstrap';
 import { FakeBackendService } from 'src/app/shared/services/fakebackend.service';
 import { UserComponent } from 'src/app/components/user/user.component';
 import { Observable } from 'rxjs';
+import { trigger, state, style, transition, animate } from '@angular/animations';
 
 
 @Component({
   selector: 'app-users',
   templateUrl: 'users.component.html',
-  styleUrls: ['users.component.css']
+  styleUrls: ['users.component.css'],
+  animations: [
+    trigger("flyInOut", [
+      state("in", style({ opacity: 1, transform: "translateY(0)" })),
+      transition("void => *", [
+        style({
+          opacity: 0,
+          transform: "translateY(-100%)"
+        }),
+        animate("0.6s ease-in")
+      ]),
+      transition("* => void", [
+        animate(
+          "0.2s ease-out",
+          style({
+            opacity: 0,
+            transform: "translateY(100%)"
+          })
+        )
+      ])
+    ])
+  ]
 })
 export class UsersComponent implements OnInit{
   @Input() users: IUser[];
